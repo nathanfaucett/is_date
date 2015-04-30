4d66d5d82eca48e7ec911b331b532ce710b16a34
Nathan Faucett <nathanfaucett@gmail.com>
2015-04-30 11:56:45 -0500

    updates

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 var isObjectLike = require("is_object_like");
 
 
-var objectDateStr = "[object Date]",
-    objectToString = Object.prototype.toString;
+var objectToString = Object.prototype.toString;
 
 
-module.exports = function isDate(obj) {
-    return (isObjectLike(obj) && objectToString.call(obj) === objectDateStr) || false;
-};
+module.exports = isDate;
+
+
+function isDate(obj) {
+    return (
+        isObjectLike(obj) &&
+        objectToString.call(obj) === "[object Date]"
+    ) || false;
+}
